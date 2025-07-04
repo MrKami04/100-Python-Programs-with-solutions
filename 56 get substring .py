c8b8cbb8490aee25b78affe322bfc0a908f1eefd
Kamran Manzoor <kamranmanzoor1448@gmail.com>
2025-07-04 10:59:59 +0500

    add file

---
+++ 56 get substring .py
@@ -0,0 +1,6 @@
+# program  that to get asubstring of a string.
+print("😍 ------------------------- 😍")
+
+string_1 = "hi i am hello and  how about you!"
+print(string_1[0:14])
+print(string_1[-14:-5])
---
+++ 57 get substring in singleline.py
@@ -0,0 +1,5 @@
+# program that to get  as substring ofa string in a single line.
+print("😍 ------------------------- 😍")
+
+print("hi hello how are your", end=" ")
+print("hello i am fine") 
