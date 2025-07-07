2077e0c7ff96ae543096653b1efb1bacc49e26d2
Kamran Manzoor <kamranmanzoor1448@gmail.com>
2025-07-08 02:00:38 +0500

    modify

--- 03 area of triangle.py
+++ 03 area of triangle.py
@@ -6,3 +6,10 @@ base = float(input("Enter the base of the triangle is:"))
 area = (0.5) * base * height
 
 print(f"Area of triangle is : {area}")
+
+# define function for find the area of triangle.
+
+def triangle_area():
+    area = (0.5) * base * height
+    return area
+print(f"area of triangle using function {triangle_area()}")
