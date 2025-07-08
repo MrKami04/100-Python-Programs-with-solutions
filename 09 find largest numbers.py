aa48d1fe265db622f2519d8276ddeb723bcaf339
Kamran Manzoor <kamranmanzoor1448@gmail.com>
2025-07-09 01:47:36 +0500

    modify

--- 09 find largest numbers.py
+++ 09 find largest numbers.py
@@ -14,3 +14,14 @@ elif number2 > number1 and number2 > number3:
     
 else:
     print(f"number3 {number3} is greatest number😀😋😊.")
+    
+# by function
+
+def largest():
+    num1 = int(input("enter the number1 here:"))
+    num2 = int(input("enter the number2 here:"))
+    num3 = int(input("enter the number3 here:"))
+    
+    model = (num1, "is greatest") if (num1 > num2 and num1 > num3) else (num2, " is greatest") if (num2 > num1 and num2 > num3) else (num3, "is greatest")
+    return model
+print(f"by function {largest()}")
