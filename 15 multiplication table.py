b3ec62e421352731f564d4ad5671838a2ad521ae
Kamran Manzoor <kamranmanzoor1448@gmail.com>
2025-07-10 22:22:20 +0500

    modify

--- 15 multiplication table.py
+++ 15 multiplication table.py
@@ -12,5 +12,13 @@ for i in range (1, 11):
 print("😀 --------------------------- 😋")
 variable_1 = 1
 while  variable_1 <= 10:
-    print(number , "X" , i , "=" , number * variable_1)
+    print(number , "X" , variable_1 , "=" , number * variable_1)
     variable_1 = variable_1 + 1
+    
+    
+# by function 
+print("😀 --------------------------- 😋")
+
+table = [f"{number} X {i} = {number * i}" for i in range(1, 11)]
+for line in table:
+    print(line)
