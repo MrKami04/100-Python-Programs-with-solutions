d8a795ddbe6e1e96c220581e931e2a5f9d9668f8
Kamran Manzoor <kamranmanzoor1448@gmail.com>
2025-07-10 11:27:43 +0500

    modify

--- 12 prime number.py
+++ 12 prime number.py
@@ -13,3 +13,12 @@ for number in range (lower_limit , upper_limit + 1):
             
         else:
             print(f"number {number} is prime number")
+            
+            
+
+print("😀 -------------------------- 😀")
+prime_numbers = [number for number in range(lower_limit, upper_limit + 1)
+                 if number > 1 and all(number % i != 0 for i in range(2, number))]
+
+for number in prime_numbers:
+    print(f"number {number} is prime number")
