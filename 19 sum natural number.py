51fc2b870ee25d14ead9a1c514e3efe0c0690aa0
Kamran Manzoor <kamranmanzoor1448@gmail.com>
2025-07-11 22:47:29 +0500

    modify

--- 19 sum natural number.py
+++ 19 sum natural number.py
@@ -6,10 +6,24 @@ number = int(input("Enter the number:"))
 if number < 0:
     print("please enter positive number.")
 else:
-    sum = 0
+    result = 0
     while number > 0:
-        sum += number
+        result += number
         number -= 1
         
-    print(f"sum of natural numbers: {sum}")    
+    print(f"sum of natural numbers: {result}")    
     
+
+print("😀 ------------------------------ 😀")
+# By function
+def sum_natural_numbers(number1):
+    result = sum([i for i in range(1, number1 + 1)]) if number1 > 0 else 0
+    print(f"sum of natural numbers: {result}")
+    
+    if number <= 0:
+        print("please enter a positive number greater than 0.")
+
+number1 = int(input("Enter a number: "))
+sum_natural_numbers(number1)
+
+
