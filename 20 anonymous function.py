1d711ee3e40e0318cd3477336d25efcaecd001e4
Kamran Manzoor <kamranmanzoor1448@gmail.com>
2025-07-12 21:55:43 +0500

    modify

--- 20 anonymous function.py
+++ 20 anonymous function.py
@@ -1,4 +1,4 @@
-# program that display power of 2 using anpnymous function
+# program that display power of 2 using anonymous function
 print("😀 ------------------------------- 😀")
 
 n_terms  = int(input("Enter number of terms here:"))
@@ -9,3 +9,11 @@ print(result)
 
 for i in range (n_terms + 1):
     print("the value of 2 raised to power", i , "is", result[i])
+    
+# by function
+print("😀 ------------------------------- 😀")
+
+def power_of_two(n_terms):
+    [print(f"the value of 2 raised to power {i} is {2**i}") for i in range(n_terms + 1)]
+
+power_of_two(n_terms)
