eaf06489239d88272a485457d7a0d5dc0118d11b
Kamran Manzoor <kamranmanzoor1448@gmail.com>
2025-07-12 22:19:09 +0500

    modify

--- 24 hcf or gcd.py
+++ 24 hcf or gcd.py
@@ -13,3 +13,11 @@ def find_hcf(variable_x, variable_y):
     return hcf 
 
 print("the HCF of the given two numbers is :", find_hcf(12,30))
+
+
+# comprehension method
+
+def find_hcf(x, y):
+    return max([i for i in range(1, min(x, y) + 1) if x % i == 0 and y % i == 0])
+
+print("The HCF of the given two numbers is:", find_hcf(12, 30))
