4351745a120292b8cf2af1bd37f638ae65ceab22
Kamran Manzoor <kamranmanzoor1448@gmail.com>
2025-07-12 22:07:00 +0500

    modify

--- 21 divisible number.py
+++ 21 divisible number.py
@@ -19,3 +19,14 @@ result = list(filter(lambda x : x % 13 ==0,number_list))
 
 print("the number divisible by 13 are:")
 print(result)
+
+
+print("😀 --------------------------------- .😆")
+#  by function
+def divisible_by_13(numbers):
+    result = [x for x in numbers if x % 13 == 0]
+    print("The numbers divisible by 13 are:")
+    print(result)
+
+number_list = [39, 48, 34, 26, 39, 33, 89, 98, 91]
+divisible_by_13(number_list)
