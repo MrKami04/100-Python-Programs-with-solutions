b0b936a972708ad70599126ee89799e4e68a220f
Kamran Manzoor <kamranmanzoor1448@gmail.com>
2025-07-13 22:27:10 +0500

    modify

--- 25 factors of number.py
+++ 25 factors of number.py
@@ -6,3 +6,15 @@ number_variable = int(input("enter the number:"))
 for i in range (1, number_variable + 1):
     if number_variable % i == 0:
         print(i)
+        
+        
+print("😀 --------------------------------- .😆")
+
+# by function
+
+def find_factors(number_variable):
+    [print(i) for i in range(1, number_variable + 1) if number_variable % i == 0]
+
+# Example usage
+
+find_factors(number_variable)
